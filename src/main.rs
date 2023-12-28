2e8b393163fade48616e45f3af41eabc837c9576
Sebastien PENCREACH <1299512+seyb@users.noreply.github.com>
2023-12-28 17:03:14 +0100

    Inits the support of mongo db

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::env;
 use axum::{Router, routing::get};
 use dotenv::dotenv;
 
+use mongodb::{Client, options::ClientOptions};
+
 mod tasks;
 
 #[tokio::main]
@@ -23,9 +25,35 @@ async fn main() {
 
     let bind_address = format!("0.0.0.0:{}", port);
 
+
+    let client = connect_mongo().await.unwrap();
+    for db_name in client.list_database_names(None, None).await.expect("Failed to list databases") {
+        println!("{}", db_name);
+    }
+
+    let db = client.database("toudou");
+
+// List the names of the collections in that database.
+    for collection_name in db.list_collection_names(None).await.expect("Failed to list collections") {
+        println!("Collection {}", collection_name);
+    }
+
+
     println!("Listening on: {}", bind_address);
     let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn connect_mongo() -> Result<Client, mongodb::error::Error> {
+    let mongo_uri = env::var("MONGODB_URI").expect("MONGO uri to be set");
+    let mut client_options = ClientOptions::parse(format!("mongodb://{}/", mongo_uri)).await.expect("Failed to create client");
+
+// Manually set an option.
+    client_options.app_name = Some("My App".to_string());
+
+// Get a handle to the deployment.
+    Client::with_options(client_options)
+    // List the names of the databases in that deployment.
 
 }
 
